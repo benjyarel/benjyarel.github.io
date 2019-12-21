77bc71861d4ce69266cd6ec482a0329bf90aa810
Benjamin Leray <b.leray35@gmail.com>
2019-12-21 16:44:58 +0100

    update for JS in stacks, neeed to solve problem of z-index before

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@
 
   const projects = document.querySelectorAll('.card-project');
 
-  projects.forEach(project => {
+  const fadeInProjects =()=>{ projects.forEach(project => {
     let triggerPosition = 600;
     let index = 0;
     let transitionProject = projects[index];
@@ -13,8 +13,26 @@
         index ++;
         transitionProject = projects[index];
          return triggerPosition = triggerPosition + 375;
-      }
+        }
+      });
     });
-  });
+};
 
   // ------------------------------------------------------------------------------
+    // JS pour afficher les stacks on clicks, ne fonctionne pas avec prb de z-index
+    // pour les clip-paths
+
+//   const stacks = document.querySelectorAll(".stack");
+//   console.log(stacks);
+
+//   const showStack = () =>{
+//     stacks.forEach(stack =>{
+//       console.log(stack);
+//       stack.addEventListener("click",(event)=> {
+//       stack.classList.toggle('show-me')
+//       });
+//     });
+//   }
+
+// showStack();
+fadeInProjects();
